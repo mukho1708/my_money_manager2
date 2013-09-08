456c852adb6fb9318a027eb835ff511d6a188f30
Abhishek Mukhopadhyay <amukhop1@server.fake>
2013-09-08 17:45:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyMoneyManager2::Application.config.secret_key_base = 'f7622124516b6d308a6622d2860a5a3a6d2f0fe7e466050734703e8ab6716609d99c159530f353466c4ac9ba90933917b8a22411c9a6caa186fd9595c942e4e1'
